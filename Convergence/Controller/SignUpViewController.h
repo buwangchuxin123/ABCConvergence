da1d39b27ff417720ebf7187d12458200ff6b0c7
PIJIAJIAN <1014310565@qq.com>
2017-09-04 14:31:23 +0800

    登录注册类

---
+++ Convergence/Controller/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  Convergence
+//
+//  Created by admin on 2017/9/4.
+//  Copyright © 2017年 EDucation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpViewController : UIViewController
+
+@end
---
+++ Convergence/Controller/SigninViewController.h
@@ -0,0 +1,13 @@
+//
+//  SigninViewController.h
+//  Convergence
+//
+//  Created by admin on 2017/9/4.
+//  Copyright © 2017年 EDucation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SigninViewController : UIViewController
+
+@end
