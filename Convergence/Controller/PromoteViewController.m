ab143a701b81ac3bbab35a0f92bca4028f47d716
PIJIAJIAN <1014310565@qq.com>
2017-09-14 17:27:26 +0800

    二维码

--- Convergence/Controller/PromoteViewController.m
+++ Convergence/Controller/PromoteViewController.m
@@ -81,34 +81,8 @@
             //   _QR = result[@"result"];
             _QR =  responseObject[@"result"];
             
-            //创建一个二维码滤镜实例（CIFilter）
-            CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
-            //滤镜恢复默认设置
-            [filter setDefaults];
-            //给滤镜添加数据
-            NSString *string = [NSString stringWithFormat:@"http://dwz.cn/%@",_QR];
-            //将字符串转成二进制数据
-            NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
-            //通过KVC设置滤镜inputMessage数据
-            [filter setValue:data forKeyPath:@"inputMessage"];
-            //4.获取生成的图片
-            CIImage *ciImg = filter.outputImage;
-            //5.设置二维码的前景色和背景颜色
-            CIFilter *colorFilter = [CIFilter filterWithName:@"CIFalseColor"];
-            //5.1设置默认值
-            [colorFilter setDefaults];
-            [colorFilter setValue:ciImg forKey:@"inputImage"];
-            [colorFilter setValue:[CIColor colorWithRed:255 green:255 blue:255] forKey:@"inputColor0"];
-            [colorFilter setValue:[CIColor colorWithRed:255 green:0 blue:255 alpha:0] forKey:@"inputColor1"];
-            //[colorFilter setValue:[UIColor redColor] forKey:@"inputColor1"];
-            // 6.获取滤镜输出的图像
-            //CIImage *outputImage = [filter outputImage];
-            ciImg = colorFilter.outputImage;
-            // 7.将CIImage转成UIImage
-            UIImage *image = [self createNonInterpolatedUIImageFormCIImage:ciImg withSize:200];
+            [self bu];
             
-            //显示二维码
-            _imageView.image = image;
         }
     } failure:^(NSInteger statusCode, NSError *error) {
         //失败以后要做的事情
@@ -122,6 +96,49 @@
 //-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
 //    
 //}
+-(void)bu{
+    //创建一个二维码滤镜实例（CIFilter）
+    CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    //滤镜恢复默认设置
+    [filter setDefaults];
+    //给滤镜添加数据
+    NSString *string = [NSString stringWithFormat:@"http://dwz.cn/%@",_QR];
+    //将字符串转成二进制数据
+    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+    //通过KVC设置滤镜inputMessage数据
+    [filter setValue:data forKeyPath:@"inputMessage"];
+    //4.获取生成的图片
+    CIImage *ciImg = filter.outputImage;
+    //5.设置二维码的前景色和背景颜色
+    CIFilter *colorFilter = [CIFilter filterWithName:@"CIFalseColor"];
+    //5.1设置默认值
+    [colorFilter setDefaults];
+    NSLog(@"%@",colorFilter.inputKeys);
+    //设置添加颜色的图片
+    [colorFilter setValue:ciImg forKey:@"inputImage"];
+    [colorFilter setValue:[CIColor colorWithRed:255 green:255 blue:255 alpha:1] forKey:@"inputColor0"];
+    [colorFilter setValue:[CIColor colorWithRed:0 green:1 blue:0 alpha:0] forKey:@"inputColor1"];
+    //[colorFilter setValue:[UIColor redColor] forKey:@"inputColor1"];
+    // 6.获取滤镜输出的图像
+    //CIImage *outputImage = [filter outputImage];
+    //获取设置完颜色的图片
+    ciImg = colorFilter.outputImage;
+    // 7.将CIImage转成UIImage
+    UIImage *image = [self createNonInterpolatedUIImageFormCIImage:ciImg withSize:200];
+    //显示二维码
+
+    UIImage *customQrcode = [self imageBlackToTransparent:image withRed:255.f andGreen:255.f andBlue:255.f];
+   _imageView.layer.shadowOffset = CGSizeMake(0, 0.5);  // 设置阴影的偏移量
+    _imageView.layer.shadowRadius = 1;  // 设置阴影的半径
+    _imageView.layer.shadowColor = [UIColor blackColor].CGColor; // 设置阴影的颜色为黑色
+    _imageView.layer.shadowOpacity = 1; // 设置阴影的不透明度
+        _imageView.image = customQrcode;
+}
+
+
+
+
+
 
 - (UIImage *)createNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat) size
 {
@@ -147,6 +164,53 @@
     return [UIImage imageWithCGImage:scaledImage];
 }
 
+
+
+
+
+#pragma mark - imageToTransparent
+void ProviderReleaseData (void *info, const void *data, size_t size){
+    free((void*)data);
+}
+//遍历图片像素
+- (UIImage*)imageBlackToTransparent:(UIImage*)image withRed:(CGFloat)red andGreen:(CGFloat)green andBlue:(CGFloat)blue{
+    const int imageWidth = image.size.width;
+    const int imageHeight = image.size.height;
+    size_t      bytesPerRow = imageWidth * 4;
+    uint32_t* rgbImageBuf = (uint32_t*)malloc(bytesPerRow * imageHeight);
+    // create context
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(rgbImageBuf, imageWidth, imageHeight, 8, bytesPerRow, colorSpace,
+                                                 kCGBitmapByteOrder32Little | kCGImageAlphaNoneSkipLast);
+    CGContextDrawImage(context, CGRectMake(0, 0, imageWidth, imageHeight), image.CGImage);
+    // traverse pixe
+    int pixelNum = imageWidth * imageHeight;
+    uint32_t* pCurPtr = rgbImageBuf;
+    for (int i = 0; i < pixelNum; i++, pCurPtr++){
+        if ((*pCurPtr & 0xFFFFFF00) < 0x99999900){
+            // change color
+            uint8_t* ptr = (uint8_t*)pCurPtr;
+            ptr[3] = red; //0~255
+            ptr[2] = green;
+            ptr[1] = blue;
+        }else{
+            uint8_t* ptr = (uint8_t*)pCurPtr;
+            ptr[0] = 0;
+        }
+    }
+    // context to image
+    CGDataProviderRef dataProvider = CGDataProviderCreateWithData(NULL, rgbImageBuf, bytesPerRow * imageHeight, ProviderReleaseData);
+    CGImageRef imageRef = CGImageCreate(imageWidth, imageHeight, 8, 32, bytesPerRow, colorSpace,
+                                        kCGImageAlphaLast | kCGBitmapByteOrder32Little, dataProvider,
+                                        NULL, true, kCGRenderingIntentDefault);
+    CGDataProviderRelease(dataProvider);
+    UIImage* resultUIImage = [UIImage imageWithCGImage:imageRef];
+    // release
+    CGImageRelease(imageRef);
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    return resultUIImage;
+}
 
 /*
 #pragma mark - Navigation
